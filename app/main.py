a5f9b066dacb11713628c411e75816e255ebd80a
Benson Domingo <benson.domingo@outlook.com>
2022-12-22 02:54:39 +0800

    cleanup main.py

--- app/main.py
+++ app/main.py
@@ -1,29 +1,8 @@
-from typing import List
-from uuid import UUID
-from fastapi import Depends, FastAPI
-from sqlalchemy import select
-from app.users.models import Gender, Profile
+from fastapi import FastAPI
 
-from pydantic import BaseModel, UUID4
-
-from .dependencies import get_sync_session, Session
+from app.users.routes import router as user_router
 
 
 app = FastAPI()
 
-
-class ProfileOutput(BaseModel):
-    id: UUID4
-    first_name: str
-    last_name: str
-
-    class Config:
-        orm_mode = True
-
-
-@app.get('/', response_model=List[ProfileOutput])
-async def read_root(db: Session = Depends(get_sync_session)):
-    stmt = select(Profile)
-    res = db.execute(stmt)
-    profiles = res.scalars().all()
-    return profiles
+app.include_router(router=user_router)
